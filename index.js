cdd077f3c3d271473c009f6b868c87943c111c4d
CarloseRochav <78558271+CarloseRochav@users.noreply.github.com>
2021-02-22 13:31:50 -0800

    Actualizacion de atribulos en la tabla 'User' Not null || Unique

--- index.js
+++ index.js
@@ -24,13 +24,13 @@ app.post("/usuarios/nuevo", (req, res) => {
   //console.log(req.body);
   console.log(User);
   User.create(req.body, {
-    name: req.body.name
-    //picture: req.body.picture,
-    // birth: req.body.birth,
-    // email: req.body.email,
-    // phone: req.body.phone,
-    // adress: req.body.adress,
-    // roll: req.body.roll
+    name: req.body.name,
+    picture: req.body.picture,
+    birth: req.body.birth,
+    email: req.body.email,
+    phone: req.body.phone,
+    adress: req.body.adress,
+    roll: req.body.roll
   })
     .then((usuario) => {
       res.send("POST / usuarios/nuevo");
@@ -64,7 +64,7 @@ app.listen(port, "0.0.0.0", () => {
 
   // Contectandose a la base de datos
   sequelize
-    .sync({ force: false })
+    .sync({ force: true })//Sincroniza el modelo con la base de datos false || true
     .then(() => {
       console.log("Conectado a la base de datos");
     })
--- sequelize/models/User.js
+++ sequelize/models/User.js
@@ -6,13 +6,37 @@ const sequelize = require("./index");
 class User extends Model {}
 User.init(
   {
-    name: DataTypes.STRING,
-    picture: DataTypes.STRING,
-    birth: DataTypes.DATE,
-    email: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    adress: DataTypes.STRING,
-    roll: DataTypes.STRING,
+    name:{                                     //Name
+      type: DataTypes.STRING,
+      allowNull:false,
+      unique:false
+        },
+      picture: DataTypes.STRING,                //Picture
+      birth:{
+      type:DataTypes.DATE,
+      allowNull:true,
+      unique:false
+            },
+      email:{                                   //Email
+        type:DataTypes.STRING,
+        allowNull:false,
+        unique:true
+            }, 
+      phone:{                                   //Phone
+        type:DataTypes.STRING,
+        allowNull:false,
+        unique:false
+            },
+        adress:{                                //Direccion
+          type:DataTypes.STRING,
+          allowNull:true,
+          unique:false          
+        },
+        roll:{                                   //Puesto/rol
+          type: DataTypes.STRING,
+          allowNull:false,
+          unique:false
+        }
   },
   {
     sequelize,
