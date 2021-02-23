4f5bbb81c01e8ae127b480799158fa511ad47738
Frank <Frank@negros-MacBook-Air.local>
2021-02-23 13:50:38 -0800

    Update User routes and controller

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const sequelize = require("./sequelize/models/index");
-const bodyParser = require("body-parser"); //Importo bodyParser
-const User = require("./sequelize/models/User"); //Importar modelo
+const bodyParser = require("body-parser"); //Import bodyParser
+const sequelize = require("./sequelize/models/");
+const { userRoutes } = require("./api/routes"); //Import Routes
 
 // Crear el servidor
 const app = express();
@@ -14,90 +14,11 @@ app.use(bodyParser.json()); //parse applicarion/json
 // habilitar cors
 app.use(cors());
 
-// Ruta a lista de Perfiles de Usuario ; GET
-app.get("/usuarios", (req, res) => {
-  User.findAll()
-    .then(
-      (usuarios) => {
-        console.log(usuarios), //Muestra en consola los usuarios
-          res.send(usuarios);
-      } //Envia usuarios registrados a a la vista}
-    )
-    .catch(console.log("Error al consultar usuarios"));
-});
-
-// Ruta para crear un nuevo usuario ; POST
-app.post("/usuarios/nuevo", (req, res) => {
-  //console.log(req.body);
-  console.log(User);
-  console.log(req.body);
-  User.create({
-    name: req.body.name,
-    picture: req.body.picture,
-    birth: req.body.birth,
-    email: req.body.email,
-    phone: req.body.phone,
-    adress: req.body.adress,
-    roll: req.body.roll,
-  })
-    .then((usuario) => {
-      res.send("POST / usuarios/nuevo");
-      console.log("INSERT SUCESSED");
-    })
-    .catch((err) => {
-      console.log("ERROR AL GUARDAR DATOS");
-    });
-});
-
-//Obtener usuario mediante id
-app.get("/usuarios/:id", (req, res) => {
-  let personaId = req.params.id;
-  User.findOne({ where: { id: personaId } })
-    .then((usuario) => {
-      res.send(usuario);//Muestra en pantalla
-      console.log(usuario);//muestra en consola
-    })
-    .catch(console.log("Error al consultar usuario"));
-});
-
-// Ruta a un perfil mediante su id ; GET
-app.post("/usuarios/:id", (req, res) => {
-  res.send("/usuarios/:id");
-});
-
-// Ruta para actualizar datos de un usuario ; PUT
-app.put("/usuarios/:id", (req, res) => {
-  let personaId = req.params.id
-  let newUsuario=req.body;
-  User.findOne({ where: { id: personaId } })
-    .then(
-      //res.send("Usuario Encontrado"),
-      (usuario) => {
-        usuario.update(newUsuario)
-          .then(newUser=>{
-            console.log("Usuario Actualizado"),
-            res.json(newUser)
-            })
-          .catch(User=>{
-            console.log("Fallo al Actualizar")
-          })
-    })
-    .catch(console.log("Error al consultar usuario, no encontrado"));
-      
-});
-
-// Ruta para eliminar usuario ; DELETE
-app.delete("/usuarios/:id", (req, res) => {
-  //res.send("/usuarios/:id");
-    let usuarioId = req.params.id;
-    User.destroy({where:{id:usuarioId}})
-      .then(()=>{
-          res.send("Usuario eliminado");
-      })
-});
-
 // puerto de la app
 const port = process.env.PORT || 8080;
+
+// Importar rutas
+app.use(userRoutes);
 
 //Arrancamos APP
 app.listen(port, "0.0.0.0", () => {
