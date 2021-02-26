becc61315552d60a3af557a58f166f099821c15b
CarloseRochav <78558271+CarloseRochav@users.noreply.github.com>
2021-02-25 18:47:24 -0800

    Validaciones crud update

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -2,28 +2,42 @@ const express = require("express");
 const router = express.Router();
 const { check } = require("express-validator");
 const { userController } = require("../controllers");
+const bodyParser = require('body-parser');
+
+//Uso de bodyP
+router.use(bodyParser.urlencoded({ extended: false })); //parse application/x-www-form-urlenconded
+router.use(bodyParser.json()); //parse applicarion/json
 
 router.get("/usuarios", userController.getUsers);
 
 router.get("/usuarios/:id", userController.getUserById);
 
 router.post(
-  "/usuarios/nuevo",
+  "/usuarios/nuevo",(req,res)=>{
   [
     check("name", "El nombre es obligatorio").not().isEmpty(),
     check("picture", "Agrega una imagen"),
     check("birth", "La fecha de nacimiento obligatorio").not().isEmpty(),
     check("email", "Agrega un correo valido").isEmail(),
     check("phone", "Agrega un telefono valido"),
-    check("address", "Agrega una direccion"),
+    check("adress", "Agrega una direccion"),
     check("roll", "El roll es obligatorio").not().isEmpty(),
-  ],
-  userController.createUser
+  ],console.log(req.body),
+  userController.createUser}
 );
 
 router.post("/usuarios/:id");
 
-router.put("/usuarios/:id", userController.updateUser);
+router.put("/usuarios/:id",[
+  check("name","No puede quedar vacio el nombre").not().notEmpty(),
+  check("picture","No olvides tu foto"),
+  check("birth","No puedes dejar este campo vacio").notEmpty.isEmpty(),
+  check("email","Recuerda que sea un correo valido").isEmail(),
+  check("phone","Asegurate que tu numero sea correcto"),
+  check("adress","Asegurate que sea una direccion valida"),
+  check("roll","Selecciona un roll").not.notEmpty(),
+
+], userController.updateUser);
 
 router.delete("/usuarios/:id", userController.deleteUser);
 
