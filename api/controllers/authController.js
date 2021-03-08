47ed1f998f1b61d3bfcdab6a0b9d337e951ad350
Frank <Frank@negros-MacBook-Air.local>
2021-03-08 13:11:32 -0800

    [Update] Validators

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { secret, expires, rounds } = require("../../config/auth");
 const { User } = require("../models/");
-const { formatError } = require("../helpers");
+const { formatError, formatMessage } = require("../helpers");
 
 exports.signUp = async (req, res) => {
   const hashPassword = bcrypt.hashSync(req.body.password, +rounds);
@@ -17,38 +17,54 @@ exports.signUp = async (req, res) => {
       address: req.body.address,
       role_id: req.body.role,
     });
-
-    res.status(201).send("The new user has been created.");
+    const messageResponse = formatMessage(
+      201,
+      "The new user has been created."
+    );
+    res.status(201).send(messageResponse);
   } catch (error) {
-    //const messageResponse = formatError(error, 500);
-    res.status(500).send(error);
+    const messageResponse = formatError(error, 500, null);
+    res.status(500).send(messageResponse);
   }
 };
 
 exports.signIn = async (req, res) => {
   const { email, password } = req.body;
-  //Find user
-  User.findOne({ where: { email: email } }).then((user) => {
+  try {
+    const user = await User.findOne({ where: { email: email } });
     if (!user) {
-      res.status(404).json({ msg: "User with this email was not found" });
-    } else {
-      if (bcrypt.compareSync(password, user.password)) {
-        const id = user.id;
-        const userPassword = user.password;
-        const token = jwt.sign(
-          { user: { id, password: userPassword, email } },
-          secret,
-          {
-            expiresIn: expires,
-          }
-        );
-        res.json({
-          user: { email, password },
-          token: token,
-        });
-      } else {
-        res.status(404).json({ msg: "User password is incorrect" });
-      }
+      const messageResponse = formatError(
+        null,
+        404,
+        "User with this email was not found"
+      );
+      throw res.status(404).json(messageResponse);
     }
-  });
+    if (bcrypt.compareSync(password, user.password)) {
+      const id = user.id;
+      const userPassword = user.password;
+      const token = jwt.sign(
+        { user: { id, password: userPassword, email } },
+        secret,
+        {
+          expiresIn: expires,
+        }
+      );
+      res.json({
+        code: 201,
+        user: { email, password },
+        token: token,
+      });
+    } else {
+      const messageResponse = formatError(
+        null,
+        500,
+        "User password is incorrect"
+      );
+      res.status(404).json(messageResponse);
+    }
+  } catch (error) {
+    const messageResponse = formatError(error, 404, null);
+    res.status(404).json(messageResponse);
+  }
 };
--- api/helpers.js
+++ api/helpers.js
@@ -1,9 +1,23 @@
-exports.formatError = (error, code) => {
+exports.formatError = (
+  error = [
+    { errors: { message: "No se ha establecido un mensaje para este error" } },
+  ],
+  code,
+  messagePersonalized = null
+) => {
   const { errors } = error;
   const { message } = errors[0];
   const response = {
     code: code,
-    message: message,
+    message: messagePersonalized ? messagePersonalized : message,
+  };
+  return response;
+};
+
+exports.formatMessage = (code, messagePersonalized = "") => {
+  const response = {
+    code: code,
+    message: messagePersonalized,
   };
   return response;
 };
